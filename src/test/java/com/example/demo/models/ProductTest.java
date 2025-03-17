7aa2d27e4a6072a559a744df1515c33bb5a71cc9
vinod-dell <68218503+vinod-dell@users.noreply.github.com>
2025-03-17 22:19:19 +0530

    Update ProductTest.java

--- src/test/java/com/example/demo/models/ProductTest.java
+++ src/test/java/com/example/demo/models/ProductTest.java
@@ -1,4 +1,4 @@
-package com.example.demo.models;
+/*package com.example.demo.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -32,5 +32,5 @@ class ProductTest {
 		
 	}
 
-}
+}*/
 
