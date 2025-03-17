5cc34c3d88ec0e28564cfc3aaf105489978c21f7
vinod-dell <68218503+vinod-dell@users.noreply.github.com>
2025-03-17 23:24:08 +0530

    Update DemoListener.java

--- src/main/java/com/example/demo/DemoListener.java
+++ src/main/java/com/example/demo/DemoListener.java
@@ -1,14 +1,11 @@
-package eu.arima.springbootwithwebxml;
+package com.example.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class DemoListener implements ServletContextListener {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(DemoListener.class);
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
